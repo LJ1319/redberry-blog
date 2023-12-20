1ba1e05236fb4c8050a43bb3c83bd779eae3de11
LJ1319 <luka.jikia.2@btu.edu.ge>
2023-12-20 22:02:31 +0400

    style: add proper styling for root layout and categories

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,11 @@
 import Header from "@/components/layout/Header.jsx";
-
 import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
 	return (
 		<div className="h-screen bg-[#f3f2fa]">
 			<Header />
-			<main>
+			<main className="bg-[#f3f2fa]">
 				<Outlet />
 			</main>
 		</div>
--- src/routes/Blogs.jsx
+++ src/routes/Blogs.jsx
@@ -1,49 +1,6 @@
 import Blog from "../../public/images/Blog.svg";
-import { useLoaderData } from "react-router-dom";
 import { axiosFetch } from "@/axios/axiosFetch.js";
-
-export default function Blogs() {
-	const { categories, blogs } = useLoaderData();
-	console.log(categories);
-	console.log(blogs);
-
-	return (
-		<div className="m-auto w-11/12">
-			<div className="my-20 flex items-center justify-between">
-				<h1 className="text-[64px] font-bold">ბლოგი</h1>
-				<img src={Blog} alt="" />
-			</div>
-			{categories.length > 0 && (
-				<div className="flex justify-center gap-3">
-					{categories.map((category) => (
-						<div key={category.id}>{category.title}</div>
-					))}
-				</div>
-			)}
-
-			{blogs.length > 0 && (
-				<div className="flex justify-center gap-3">
-					{blogs.map((blog) => (
-						<div key={blog.id}>
-							<img src={blog.image} alt="" />
-							<p>{blog.title}</p>
-							<p>{blog.description}</p>
-							<p>{blog.author}</p>
-							<p>{blog.publish_date}</p>
-							<div className="flex justify-between gap-3">
-								{blog.categories.map((category) => (
-									<p key={category.id}>{category.title}</p>
-								))}
-							</div>
-
-							<p>{blog.email}</p>
-						</div>
-					))}
-				</div>
-			)}
-		</div>
-	);
-}
+import { useLoaderData } from "react-router-dom";
 
 export async function loader() {
 	const categoriesResponse = await axiosFetch("/categories");
@@ -67,4 +24,55 @@ export async function loader() {
 	}
 
 	return { categories, blogs };
+}
+
+export default function Blogs() {
+	const { categories, blogs } = useLoaderData();
+	console.log(categories);
+	console.log(blogs);
+
+	return (
+		<div className="m-auto w-11/12 py-16">
+			<div className="flex items-center justify-between">
+				<h1 className="text-[64px] font-bold">ბლოგი</h1>
+				<img src={Blog} alt="" />
+			</div>
+			{categories.length > 0 && (
+				<div className="my-16 flex justify-center gap-3">
+					{categories.map((category) => (
+						<button
+							key={category.id}
+							style={{
+								color: category.text_color,
+								backgroundColor: category.background_color,
+							}}
+							className="h-8 rounded-full p-2 text-xs"
+						>
+							{category.title}
+						</button>
+					))}
+				</div>
+			)}
+
+			{blogs.length > 0 && (
+				<div className="flex justify-center gap-3">
+					{blogs.map((blog) => (
+						<div key={blog.id}>
+							<img src={blog.image} alt="" />
+							<p>{blog.title}</p>
+							<p>{blog.description}</p>
+							<p>{blog.author}</p>
+							<p>{blog.publish_date}</p>
+							<div className="flex justify-between gap-3">
+								{blog.categories.map((category) => (
+									<p key={category.id}>{category.title}</p>
+								))}
+							</div>
+							<p>{blog.email}</p>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
