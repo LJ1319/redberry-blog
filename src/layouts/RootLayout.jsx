ece4f6d91695fcc8446d28f1a36e34d9c2cafebd
LJ1319 <luka.jikia.2@btu.edu.ge>
2023-12-28 19:44:24 +0400

    style: change main background according to url

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,24 @@
 import Header from "@/components/Header.jsx";
-import { Outlet, ScrollRestoration } from "react-router-dom";
+import { Outlet, ScrollRestoration, useLocation } from "react-router-dom";
+import { classNames } from "@/helpers.js";
 
 export default function RootLayout() {
+	const location = useLocation();
+
 	return (
-		<div className="h-screen bg-[#f3f2fa] text-[#1A1A1F]">
+		<div
+			className={classNames(
+				location.pathname === "/create" ? "bg-[#fbfaff]" : "bg-[#f3f2fa]",
+				"h-screen text-[#1A1A1F]",
+			)}
+		>
 			<Header />
-			<main className="bg-[#f3f2fa] py-20">
+			<main
+				className={classNames(
+					location.pathname === "/create" ? "bg-[#fbfaff]" : "bg-[#f3f2fa]",
+					"py-20",
+				)}
+			>
 				<Outlet />
 			</main>
 			<ScrollRestoration />
