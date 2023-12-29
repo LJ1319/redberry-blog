5d0658ba765dd9676d876f316acd88dc31acf7cb
LJ1319 <luka.jikia.2@btu.edu.ge>
2023-12-29 09:46:28 +0400

    style: fix image error message alignment

--- src/routes/Create.jsx
+++ src/routes/Create.jsx
@@ -184,42 +184,44 @@ export default function Create() {
 										</button>
 									</div>
 								) : (
-									<div
-										className={classNames(
-											imageError
-												? "border-[#EA1919] bg-[#FAF2F3]"
-												: "border-[#85858D] bg-[#F4F3FF]",
-											"relative h-52 w-full rounded-xl border border-dashed ",
-										)}
-									>
-										<label
-											className="my-2 flex h-full w-full flex-col items-center justify-center gap-y-6 py-12"
-											htmlFor="image"
+									<>
+										<div
+											className={classNames(
+												imageError
+													? "border-[#EA1919] bg-[#FAF2F3]"
+													: "border-[#85858D] bg-[#F4F3FF]",
+												"relative w-full rounded-xl border border-dashed",
+											)}
 										>
-											<img src={AddFileIcon} alt="Add File Icon" />
-											<span className="text-sm">
-												ჩააგდეთ ფაილი აქ ან{" "}
-												<span className="font-bold underline">
-													აირჩიეთ ფაილი
+											<label
+												className="my-2 flex h-full w-full flex-col items-center justify-center gap-y-6 py-12"
+												htmlFor="image"
+											>
+												<img src={AddFileIcon} alt="Add File Icon" />
+												<span className="text-sm">
+													ჩააგდეთ ფაილი აქ ან{" "}
+													<span className="font-bold underline">
+														აირჩიეთ ფაილი
+													</span>
 												</span>
-											</span>
-										</label>
-										<input
-											type="file"
-											className="absolute top-0 h-full w-full cursor-pointer opacity-0"
-											id="image"
-											accept="image/*"
-											onChange={imageUploadHandler}
-										/>
+											</label>
+											<input
+												type="file"
+												className="absolute top-0 h-full w-full cursor-pointer opacity-0"
+												id="image"
+												accept="image/*"
+												onChange={imageUploadHandler}
+											/>
+										</div>
 										{imageError && (
-											<div className="flex h-5 items-center gap-2">
+											<div className="my-2 flex h-5 items-center gap-2">
 												<img src={InfoIcon} alt="Info Icon" />
 												<p className="text-xs text-[#EA1919]">
 													არასწორი ფორმატი
 												</p>
 											</div>
 										)}
-									</div>
+									</>
 								)}
 							</div>
 							<div className="flex justify-between gap-6">
